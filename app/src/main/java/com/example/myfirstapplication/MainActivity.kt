4cb7a549bf1c97c450fb137cd9aafc7b7d4b713d
gugutze <gugutze@ngs.ru>
2021-12-08 00:45:52 +0700

    Button with ClickListener and Toast

--- app/src/main/java/com/example/myfirstapplication/MainActivity.kt
+++ app/src/main/java/com/example/myfirstapplication/MainActivity.kt
@@ -2,10 +2,31 @@ package com.example.myfirstapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var helloTextView: TextView
+    lateinit var newTextButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        helloTextView = findViewById(R.id.hello_world_text_view)
+        newTextButton = findViewById(R.id.new_text_button)
+
+        newTextButtonClick()
+
     }
+
+    fun newTextButtonClick() {
+        newTextButton.setOnClickListener {
+            Toast.makeText(this, "Hello", Toast.LENGTH_LONG).show()
+        }
+    }
+
 }
